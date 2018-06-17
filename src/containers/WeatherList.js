24b7eed5ab630f047e356ebc86e0b4cf73be0db0
daria marczak <marczak.ria@gmail.com>
2018-06-17 18:12:20 +0200

    Added charts for each column

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Chart from "../components/Chart";
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    
     return (
       <tr key={name}>
-        <td key={name}>{cityData.city.name}</td>
+        <td>{name}</td>
+        <td><Chart data={temps} color="blue" units="K"/></td>
+        <td><Chart data={pressure} color="green" units="hPa"/></td>
+        <td><Chart data={humidity} color="orange" units="%"/></td>        
       </tr>
     )
   };
